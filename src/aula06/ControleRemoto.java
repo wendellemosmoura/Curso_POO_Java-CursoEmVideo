0f391eb91e9f57ee42de43d335663812c458612e
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-11-02 22:44:55 -0300

    aula06 - Encapsulamento.

---
+++ src/aula06/ControleRemoto.java
@@ -0,0 +1,107 @@
+package aula06;
+
+public class ControleRemoto implements Controlador{
+    // Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    // Métodos especiais
+    public ControleRemoto() {
+        volume = 50;
+        ligado = false;
+        tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean getTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    // Métodos abstratos
+    @Override
+    public void ligar() {
+        setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + getLigado());
+        System.out.println("Está tocando? " + getTocando());
+        System.out.print("Volume" + getVolume());
+        for (int i =0; i <= getVolume(); i+=10) {
+            System.out.print("[]");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando Menu...");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (getLigado()) {
+            setVolume(getVolume() + 5);
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (getLigado()) {
+            setVolume(getVolume() - 5);
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (getLigado() && getVolume() > 0) {
+            setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligarMudo() {
+        if (getLigado() && getVolume() == 0) {
+            setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (getLigado() && !(getTocando())) {
+            setTocando(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (getLigado() && getTocando()) {
+            setTocando(false);
+        }
+    }
+}
