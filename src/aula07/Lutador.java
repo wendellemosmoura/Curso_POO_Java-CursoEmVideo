87a08c0e961acc948ef9c70f2beb7cfc8c77d6ae
wendellemosmoura <wendel_lemos_cg@hotmail.com>
2022-11-02 22:59:22 -0300

    aula07a - Relacionamento entre classes.

---
+++ src/aula07/Lutador.java
@@ -0,0 +1,48 @@
+package aula07;
+
+public class Lutador {
+    // Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    // Métodos
+    public void apresentar() {
+
+    }
+
+    public void status() {
+
+    }
+
+    public void ganharLuta() {
+
+    }
+
+    public void perderLuta() {
+
+    }
+
+    public void empatarLuta() {
+
+    }
+
+    // Metodos especiais
+
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+}
---
+++ src/aula07/Main.java
@@ -0,0 +1,7 @@
+package aula07;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
