c667e2ef68343e85d3abb0153261e7a5d67b12d7
wendel.lemos <wendel_lemos_cg@hotmail.com>
2022-11-07 09:07:54 -0300

    Aula14 - Projeto final parte 2.

--- src/aula14/Main.java
+++ src/aula14/Main.java
@@ -12,7 +12,16 @@ public class Main {
         g[0] = new Gafanhoto("Wendel", 37, "M", "wlm");
         g[1] = new Gafanhoto("Melina", 3, "F", "mel");
 
-        System.out.println(v[0]);
-        System.out.println(g[0]);
+        Visualizacao[] vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[1], v[0]);
+        vis[1] = new Visualizacao(g[1], v[1]);
+        vis[2] = new Visualizacao(g[1], v[2]);
+
+//        System.out.println(v[0]);
+//        System.out.println(g[0]);
+
+        System.out.println(vis[0]);
+        System.out.println(vis[1]);
+        System.out.println(vis[2]);
     }
 }
